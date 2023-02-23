bc055f5308a02289094980c6e2c7446154bbd9bd
Kyevnieves <keyvernievess@gmail.com>
2023-02-23 13:46:12 -0430

    StorageImgUser

--- src/lib/multer.js
+++ src/lib/multer.js
@@ -1,12 +1,14 @@
 const multer = require("multer");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+
 const storage = multer.diskStorage({
   destination: path.join(__dirname, "../public/images/products"),
   filename: (req, file, cb) => {
     cb(null, uuidv4() + path.extname(file.originalname));
   },
 });
+
 const upload = multer({
   storage: storage,
   dest: path.join(__dirname, "public/images/products"),
@@ -24,8 +26,15 @@ const upload = multer({
   },
 }).single("image");
 
+const storageImgUser = multer.diskStorage({
+  destination: path.join(__dirname, "../public/images/users"),
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + path.extname(file.originalname));
+  },
+});
+
 const uploadUserImage = multer({
-  storage: storage,
+  storage: storageImgUser,
   dest: path.join(__dirname, "public/images/users"),
   limits: {
     fileSize: 1500000,
